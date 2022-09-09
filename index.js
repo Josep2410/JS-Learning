a25269cd1e02af720ae77093bd635122dea693ed
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-09 16:44:44 -0500

    random number generator

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+let x;
+let y;
+let z;
 
-let count =0;
+document.getElementById("rollbutton").onclick = function(){
 
-document.getElementById("decreaseBtn").onclick = function(){
-  count-=1;
-  document.getElementById("countLabel").innerHTML =count;
-}
+    x = Math.floor(Math.random() * 6) + 1;
+    y = Math.floor(Math.random() * 6) + 1;
+    z = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById("resetBtn").onclick = function(){
-  count=0;
-  document.getElementById("countLabel").innerHTML =count;
-}
-
-document.getElementById("increaseBtn").onclick = function(){
-  count+=1;
-  document.getElementById("countLabel").innerHTML =count;
+    document.getElementById("xlabel").innerHTML = x;
+    document.getElementById("ylabel").innerHTML = y;
+    document.getElementById("zlabel").innerHTML = z;
 }
