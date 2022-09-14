c426dcaf0015284f534ea54db06fb4ded52ac38f
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-14 14:22:18 -0500

    spread operator

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// 2D array = An array of arrays
+// spread operator = allows an iterable such as an 
+// ...                             array or string to be expanded 
+//                                 in places where zero or more 
+//                                 arguments are expected
+//                                (unpacks the elements)
 
-let fruits = ['apples', 'oranges', 'bananas'];
-let vegetables = ["carrots", "onions", "potatoes"];
-let meats =         ["eggs", "chicken", "fish"];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let maximum = Math.max(...numbers);
+console.log(maximum);
 
-let groceryList = [fruits, vegetables, meats]
+/*
+let class1 = ["Spongebob", "Patrick", "Sandy"];
+let class2 = ["Squidward", "Mr.Krabs", "Plankton"];
+class1.push(...class2);
+console.log(...class1);
+*/
