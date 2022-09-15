c9e29031ec532f769fff97901ab739c9ba9e9a04
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-15 18:39:55 -0500

    arrow functs, funct expressions, rest parameters

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-document.getElementById("multiply").onclick = function(){
-  let product;
-  firstInput = Number(document.getElementById("firstInput").value);
-  secondInput = Number(document.getElementById("secondInput").value);
-  product = ( firstInput* secondInput);
-  document.getElementById("resultDisplay").innerHTML = product;
+// arrow function expression = compact alternative to a traditional function expression
+//      =>
+
+const grades = [100, 50, 90, 60, 80, 70];
+
+grades.sort((x, y) => y - x);
+grades.forEach((element) => console.log(element));
+
+const word = 'a million dollars';
+let message = "whats up didily doers";
+
+//TRADITIONAL function
+/* function greeting (message){
+  console.log(message);
+} */
+
+//FUNCTION expression
+/* const greeting = function(message){
+  console.log(message);
+} */
+
+//arrow function expression
+const greeting = message => {
+  console.log(`${word} finally`);
+}
+greeting(message);
+
+
+//combining rest parameters and function expressions
+
+ const multiply = function(...multiplicands){
+  let product = 1 ;
+  for(let item of multiplicands){
+    product*=item;
+  }
+  return product;
 }
 
-document.getElementById("divide").onclick = function(){
-  let answer;
-  firstInput = Number(document.getElementById("firstInput").value);
-  secondInput = Number(document.getElementById("secondInput").value);
-  answer = ( firstInput / secondInput);
-  document.getElementById("resultDisplay").innerHTML = answer;
-}
-  
+console.log(multiply( 2 , 10 , 14 ));
+
 
