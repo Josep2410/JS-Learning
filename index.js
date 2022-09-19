74f7e544d501ba4579572d38d4fc0d68ada82d9d
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-19 16:53:37 -0500

    closures

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-// nested functions = Functions inside other functions.
-//                                   Outer functions have access to inner functions.
-//                                   Inner functions are "hidden" from outside.
-//                                   used in closures (future video topic)
- 
-let userName = "Bro";
-let userInbox = 0;
+// closure =  A function with preserved and private data.
+//                   Gives you access to an outer function’s scope,
+//                   from an inner function.
 
-login();
+document.getElementById("loginButton").onclick = login();
+
+//userInbox = 420.69;
 
 function login(){
-    displayUserName();
-    displayUserInbox();
+    let userName = "Bro";
+    let userInbox = 1;
 
-    function displayUserName(){
-        console.log(`Welcome ${userName}`);
+    function alertUser(){
+        alert(`${userName} you have ${userInbox} new messages!`);
+        userInbox = 0;
     }
-    function displayUserInbox(){
-        console.log(`You have ${userInbox} new messages`);
-    }
+
+    return alertUser;
 }
-
-//things to remember:
-/* 
-- global variable is used. thats how each inner function can access the userName
-   and userInbox variable
-- You have to call a function in order to use it. Simply wrting out the function
-   will not invoke the function => call it.
-- Using nested functions add a bit of security.
- */
