7e971d5120f3605f458fac31577ece034290011a
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-21 15:54:10 -0500

    anonymous objects

--- index.js
+++ index.js
@@ -1,66 +1,17 @@
-// get = binds an object property to a function 
-//          when that property is accessed
-// set = binds an object property to a function
-//          when that property is assigned a value
-
-class Car{
-  constructor(power){
-      this._gas = 25;
-      this._power = power;
-  }
-  get power(){
-      return `${this._power}hp`;
-  }
-  get gas(){
-      return `${this._gas}L (${this._gas / 50 * 100}%)`;
-  }
-  set gas(value){
-      if(value > 50){
-          value = 50;
-      }
-      else if(value < 0){
-          value = 0;
-      }
-      this._gas = value;
+class Card{
+  constructor(value, suit){
+      this.value = value;
+      this.suit = suit;
   }
 }
 
-let car = new Car(400);
+let cards = [new Card("A", "Hearts"), 
+           new Card("A", "Spades"), 
+           new Card("A", "Diamonds"), 
+           new Card("A", "Clubs"), 
+           new Card("2", "Hearts"), 
+           new Card("2", "Spades"), 
+           new Card("2", "Diamonds"), 
+           new Card("2", "Clubs")];
 
-/* car.gas = 100; */
-
-console.log(car.power);
-console.log(car.gas);
-
-class Phone {
-  constructor(price){
-    this._price = price;
-    this._battery = 8;
-  }
-  get price(){
-    return `$${this._price}`;
-  }
-
-  get battery(){
-      return `Your phone is at ${(this._battery / 8) *100}%`;
-  }
-
-  set battery(usage){
-    if(usage > 8)
-      usage = 8;
-    else if( usage < 0)
-      usage = 0;
-    this._battery = usage;
-  }
-}
-
-let userInput = window.prompt("How long have you used your phone today? ENTER in units of hours: ");
-userInput = Number(userInput);
-
-const iphone8 = new Phone(400);
-
-iphone8.battery = userInput;
-
-console.log(iphone8.price);
-
-console.log(iphone8.battery);
+cards.forEach(card => console.log(`${card.value} ${card.suit}`));
