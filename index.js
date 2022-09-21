b38b1391b5e592ba3ff81b04ccd0f976d9973614
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-21 17:35:05 -0500

    setInterval( )

--- index.js
+++ index.js
@@ -1,68 +1,15 @@
-// setTimeout() = invokes a function after a number of milliseconds            
-//                            asynchronous function (doesn't pause execution) 
+// setInterval() = invokes a function repeatedly after no. of milliseconds
+//                          asynchronous function (doesn't pause execution) 
 
-/* let item = "cryptocurrency";
-let price = 420.69;
+let count = 0;
+let max = window.prompt("Count up to what #?");
+max = Number(max);
+const myTimer = setInterval(countUp, 1000);
 
-let timer1 = setTimeout(firstMessage, 3000, item, price);
-let timer2 = setTimeout(secondMessage, 6000);
-let timer3 = setTimeout(thirdMessage, 9000);
-
-function firstMessage(item, price){
-    alert(`Buy this ${item} for $${price}`);
-}
-function secondMessage(){
-    alert(`This is not a scam!`);
-}
-function thirdMessage(){
-    alert(`DO ITT!`);
-}
-document.getElementById("myButton").onclick = function(){
-    clearTimeout(timer1); 
-    clearTimeout(timer2);
-    clearTimeout(timer3);
-    alert(`Thanks for buying!`);
-}
- */
-
-
-
-
-
-
-
-/* 
-   ================= Ad starter and stopper program ================
- */
-
-   let price = 4.99
-document.getElementById("ad").onclick = function(){
-  let timer1 = firstMessage(price);
-  let timer2 = setTimeout(secondMessage, 2000);
-  let timer3 = setTimeout(thirdMessage, 6000);
-  let timer4 = setTimeout(fourthMessage, 10000);
-
-  document.getElementById("myButton").onclick = function(){
-    clearTimeout(timer1); 
-    clearTimeout(timer2);
-    clearTimeout(timer3);
-    clearTimeout(timer4);
-  }
-}
-
-function firstMessage(price){
-  alert(`Buy enlargement pills for $${price}`);
-}
-
-function secondMessage(){
-  alert(`Make your woman happy with this simple trick`);
-}
-
-function thirdMessage(){
-  alert(`Dont miss out`);
-
-}
-function fourthMessage(){
-  alert(`The industry does not want you to know this!`);
-  
+function countUp(){
+    count+=1;
+    console.log(count);
+    if(count >= max){
+        clearInterval(myTimer);
+    }
 }
