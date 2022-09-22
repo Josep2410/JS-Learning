9e1cd6cdff72a94912032551710b8fc8cc3ced41
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-22 13:37:08 -0500

    Clock Program

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-// The Date object is used to work with dates & times
+const myLabel = document.getElementById("myLabel");
 
-let date = new Date();
-//let date = new Date(0);
-//let date = new Date(2023, 0, 1, 2, 3, 4, 5);
-//let date = new Date("January 1, 2023 00:00:00");
-/*
-let year = date.getFullYear();
-let dayOfMonth = date.getDate();
-let dayOfWeek = date.getDay();
-let month = date.getMonth();
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-let ms = date.getMilliseconds();
-/
-/
-date.setFullYear(2024);
-date.setMonth(11);
-date.setDate(31);
-date.setHours(23);
-date.setMinutes(1);
-date.setSeconds(30);
-date.setMilliseconds(0);
-*/
+update();
+setInterval(update, 1000);
 
-date = date.toLocaleString();
-document.getElementById("myLabel").innerHTML = date
+function update(){
+
+    let date = new Date();
+    myLabel.innerHTML = formatTime(date);
+
+    function formatTime(date){
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let amOrPm = hours >= 12 ? "pm" : "am";
+
+        hours = (hours % 12) || 12;
+
+        hours = formatZeroes(hours);
+        minutes = formatZeroes(minutes);
+        seconds = formatZeroes(seconds);
+
+        return `${hours}:${minutes}:${seconds} ${amOrPm}`;
+    }
+    function formatZeroes(time){
+        time = time.toString();
+        return time.length < 2 ? "0" + time : time;
+    }
+}
