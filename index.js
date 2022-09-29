c3f7e22104f65ed49d45549e4141d0a821281d50
Joseph Moreno <josephpinoy2@yahoo.com>
2022-09-29 17:10:10 -0500

    animations

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-window.addEventListener("keydown", event => console.log(event.key));
-const myDiv = document.getElementById("myDiv");
-window.addEventListener("keydown", move);
-let x = 0;
-let y = 0;
+const myButton = document.getElementById("myButton");
+const myAnimation = document.getElementById("myDiv");
 
-function move(event){
-    
-    switch(event.key){
-        case "ArrowDown":
-            y+=5;
-            myDiv.style.top = y + "px";
-            break;
-        case "ArrowUp":
-            y-=5;
-            myDiv.style.top = y + "px";
-            break;
-        case "ArrowRight":
-            x+=5;
-            myDiv.style.left = x + "px";
-            break;
-        case "ArrowLeft":
-            x-=5;
-            myDiv.style.left = x + "px";
-            break;
-        default:
-            break;
+myButton.addEventListener("click", slide);
+//myButton.addEventListener("click", rotate);
+//myButton.addEventListener("click", scale);
+
+function slide(){
+    let timerId = null;
+    let x = 0;
+    let y = 0;
+
+    timerId = setInterval(frame, 5);
+
+    function frame(){
+        if(x >= 200 || y >= 200){
+            clearInterval(timerId);
+        }
+        else{
+            x+=1;
+            y+=1;
+            myAnimation.style.top = y + 'px';
+            myAnimation.style.left = x + 'px';
+        }
+    }
+}
+function rotate(){
+    let timerId = null;
+    let degrees = 0;
+ 
+    timerId = setInterval(frame, 5);
+
+    function frame(){
+        if(degrees >= 360){
+            clearInterval(timerId);
+        }
+        else{
+            degrees+=1;
+            myAnimation.style.transform = "rotateZ("+degrees+"deg)";
+        }
+    }
+}
+function scale(){
+    let timerId = null;
+    let scaleX = 1;
+    let scaleY = 1;
+ 
+    timerId = setInterval(frame, 5);
+
+    function frame(){
+        if(scaleX <= 0.1 || scaleY <= 0.1){
+            clearInterval(timerId);
+        }
+        else{
+            scaleX-=0.01;
+            scaleY-=0.01;
+            myAnimation.style.transform = "scale("+scaleX+","+scaleY+")";
+        }
     }
 }
