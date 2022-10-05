689f888a5bf3f4b57dd85e4ed96ca05d1fa2fc65
Joseph Moreno <josephpinoy2@yahoo.com>
2022-10-05 14:43:36 -0500

    window object

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
-let canvas = document.getElementById("myCanvas");
-let context = canvas.getContext("2d");
 
 
-/* //DRAW LINES
-context.strokeStyle = "purple"; //determine color of drawn line
-context.lineWidth = 5; //Determine the thickness of the drawn lines
-context.beginPath();
-context.moveTo(250,0);
-context.lineTo(250,250);
-context.moveTo(0,0);
-context.lineTo(250,250);
-context.lineTo(250,500);
-context.moveTo(500,0);
-context.lineTo(250,250);
-context.stroke(); */
+// window = interface used to talk to the web browser
+//                   the DOM is a property of the window
 
-/* //DRAW TRIANGLE
-context.strokeStyle = 'blue'; //determine color of drawn line
-context.fillStyle = 'pink'; //determine what color to fill shape
-context.lineWidth = 5; //Determine the thickness of the drawn lines
-context.beginPath();
-context.moveTo(10,490);
-context.lineTo(250,10);
-context.lineTo(490,490);
-context.lineTo(10,490);
-context.stroke();
-context.fill(); */
+const myButton = document.querySelector("#myButton");
 
-/* //DRAW RECTANGLE
-context.fillStyle = 'pink'; 
-context.fillRect(0,0, 250, 250);
-context.strokeStyle = "black";
-context.strokeRect(0, 0, 250, 250);
+//console.dir(window);
+//console.log(window.innerWidth);
+//console.log(window.innerHeight);
+//console.log(window.scrollX);
+//console.log(window.scrollY);
 
-context.fillStyle = "red";
-context.fillRect(0, 250, 250, 250);
-context.strokeStyle = "black";
-context.strokeRect(0, 250, 250, 250);
+//console.log(window.location.href);
+//window.location.href = "https://google.com";
+//console.log(window.location.hostname);
+//console.log(window.location.pathname);
 
-context.fillStyle = "green";
-context.fillRect(250, 250, 250, 250);
-context.strokeStyle = "black";
-context.strokeRect(250, 250, 250, 250);
+//myButton.addEventListener("click", () => window.open("https://google.com"));
+//myButton.addEventListener("click", () => window.close());
+//myButton.addEventListener("click", () => window.print());
 
-context.fillStyle = "blue";
-context.fillRect(250, 0, 250, 250);
-context.strokeStyle = "black";
-context.strokeRect(250, 0, 250, 250); */
-
-/* //DRAW CIRCLE
-//(x, y, r, sAngle, eAngle, counterclockwise)
-context.fillStyle = "lightblue";
-context.strokeStyle = "darkred";
-context.lineWidth = 5;
-context.beginPath();
-context.arc(0, 0, 100, 5 / 3 * Math.PI, 11 / 6 * Math.PI, true );
-context.stroke();
-context.fill(); */
-
-//DRAW TEXT
-context.font = "50px MV Boli";
-context.fillStyle = "grey";
-context.textAlign = "center";
-context.fillText("YOU WIN!", canvas.width / 2, canvas.height / 2);
+//window.alert("Hello!");
+//window.confirm("Press OK to coninue!");
+/*
+let age = window.prompt("Enter your age");
+if(age < 18){
+    window.alert("You must be 18+ to visit this site");
+    window.close();
+}
+*/
