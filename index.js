9ace1d2c5fcb1ea10c4ca44fd8b098180f87c857
Joseph Moreno <josephpinoy2@yahoo.com>
2022-10-20 14:54:02 -0500

    array.filter()

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
+//array.filter() = creates a new array with all elements 
+//                         that pass the test provided by a function
 
+let ages = [18, 16, 21, 17, 19, 90];
+let adults = ages.filter(checkAge);
 
-//array.map() = executes a provided callback function
-//                         once for each array element
-//                         AND creates a new array
+adults.forEach(print);
 
-//array.map()
-let numbers = [1, 2, 3, 4, 5];
-let squares = numbers.map(square);
-let cubes = numbers.map(cube);
-
-//For Each method
-numbers.forEach((element) =>{
-  console.log( Math.pow(element,2));
-})
-
-//numbers.forEach(print);
-//squares.forEach(print);
-//cubes.forEach(print);
-
-function square(element){
-    return Math.pow(element, 2);
-}
-function cube(element){
-    return Math.pow(element, 3);
+function checkAge(element){
+    return element >= 18;
 }
 function print(element){
     console.log(element);
