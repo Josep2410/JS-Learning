8fc87fec84ecbe3684bd411a177ccc9f39f8c40d
Joseph Moreno <josephpinoy2@yahoo.com>
2022-10-31 18:55:53 -0500

    transitions w/ multiple cards

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const show = document.querySelector(".on");
-const p = document.querySelector("p");
-const off = document.querySelector(".off");
+const block = document.querySelector(".block");
+const p1 = document.querySelector(".p1");
+const block2 = document.querySelector(".block2");
+const p2 = document.querySelector(".p2");
 
-show.addEventListener("click", function(){
-    p.classList.toggle("p-display");
-    off.classList.toggle("off-display");
-    show.classList.toggle("on-display");
+block.addEventListener("click", function(){
+    p1.classList.toggle("p-display");
+    block.classList.toggle("rotate");
 })
 
-off.addEventListener("click", function(){
-    p.classList.toggle("p-display");
-    off.classList.toggle("off-display");
-    show.classList.toggle("on-display");
+block2.addEventListener("click", function(){
+    p2.classList.toggle("p-display2");
+    block2.classList.toggle("rotate");
 })
+
+
+
