051c25947a7e0dca0c2c24e1118aa9224e663edc
Joseph Moreno <josephpinoy2@yahoo.com>
2022-11-30 22:06:12 -0600

    spread and rest operator

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-let btn = document.querySelectorAll("#watch");
-let word = 'word hello';
-console.log(word.substring(3.6));
-const words = document.querySelector("#text");
+/* Spread Operator / Rest Operator */
 
-const sub = document.querySelector("#sub-text");
-btn.forEach(item =>{
-  item.addEventListener("click", function(){
-    words.textContent = rotate();
-    words.append(item.value);
-   sub.textContent.substring(0,1) = item.value;
-  
-  })
-})
+// add the elements of an existing array into a new array
+var certsToAdd = ['Algorithms and Data Structures', 'Front End Libraries']; 
+var certifications = ['Responsive Web Design', ...certsToAdd, 'Data Visualization', 'APIs and Microservices', 'Quality Assurance and Information Security'];
+console.log(certifications);
 
-function rotate(){
-  return words.textContent.length>1? words.textContent.substring(1,2) : words.textContent;
+let elements = [1,2,3];
+let num = [4,5,6];
+console.log(num);
+num = [elements, ...num];
+console.log(num);
+
+
+// pass elements of an array as arguments to a function
+function addThreeNumbers(x, y, z) { 
+	console.log(x+y+z)
+}
+var args = [0, 1,10];
+addThreeNumbers(...args);
+
+function welcome(m1,m2,m3){
+  console.log(`${m1} ${m2} ${m3}  `);
+}
+let text = ['how', 'are', 'you'];
+welcome(...text);
+
+// copy arrays
+var arr = [1, 2, 3];
+var arr2 = [...arr]; // like arr.slice()
+arr2.push(4); 
+console.log(arr);
+console.log(arr2);
+
+let text2 = [...text];
+console.log(text2);
+
+// REST: condense multiple elements into an array
+function multiply(multiplier, ...theArgs) {
+  return theArgs.map(function(element) {
+    return multiplier * element;
+  });
 }
 
+var arr = multiply(2, 1, 2, 3); 
+console.log(arr)
+
+function sum(sum, ...nums){
+  
+  nums.forEach(element =>{
+    sum+=element;
+  })
+  return sum;
+}
+console.log(sum(6,1,2,3));
